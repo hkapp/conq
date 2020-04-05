a0385eccd769b1a97f5e39b218961c0352725ff1
Hugo Kapp <hugo.kapp@epfl.ch>
2020-04-05 13:44:56 +0200

    Introduce failure explanation in ParserTest

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,6 @@
 module Main where
 
-import Parser
-import RegexParser
 import ParserTest
 
 main:: IO ()
 main = runAllTests
-    --do
-    --let str = "a|a|a"
-        --parseResult = match str
-    --putStrLn ((enclose str) ++ (explain parseResult))
-
-
-enclose str = '"' : str ++ '"' : []
-
-explain :: ParseResult t -> String
-explain (Success t [])  = " isValidRegex completely"
-explain (Success t rem) = " partly isValidRegex, rem=" ++ (enclose rem)
-explain Failure         = " does not match"
--- src/ParserTest.hs
+++ src/ParserTest.hs
@@ -1,23 +1,20 @@
 module ParserTest where
 
-import RegexParser
+import RegexParser(isValidRegex)
 import RegexOpTree
 import RegexEval
 import Data.Bool(bool)
 
-data Test = Test String Bool
+data TestResult = Success | Failure (Maybe String)
+data Test = Test String TestResult
 data TestSuite = TestSuite String [Test]
 type TestLib i o = String -> i -> o -> Test
 data TestReport = TestReport Int Int
 
--- New test infrastructure
+-- Exported 'run' utilities
 
-basicTestLib :: (i -> Bool) -> i -> String -> Test
-basicTestLib f input name = Test name (f input)
-
-basicAssertLib :: (Eq o) => (i -> o) -> o -> i -> String -> Test
-basicAssertLib f expectedOutput = basicTestLib assertResult
-  where assertResult input = (f input == expectedOutput)
+runAllTests :: IO ()
+runAllTests = runSuite parserSuite
 
 runSuite :: TestSuite -> IO ()
 runSuite (TestSuite name tests) = do
@@ -36,12 +33,67 @@ runSuite (TestSuite name tests) = do
 runTest :: Test -> IO (Bool)
 runTest test@(Test name result) = do
   printTestResult test
-  return result
+  return (isSuccess result)
+
+isSuccess :: TestResult -> Bool
+isSuccess Success = True
+isSuccess (Failure _) = False
+
+-- Printers
+
+printTestResult :: Test -> IO ()
+printTestResult (Test name result) = do
+  putStrLn ((testLinePrefix (isSuccess result)) ++ sep ++ name)
+  printExplanation result
+    where sep = " "
+          testLinePrefix True = "[OK]"
+          testLinePrefix False = "[XX]"
+          printExplanation (Failure (Just explanation)) = putStrLn explanation
+          printExplanation _ = return ()
+
+printSuiteStart :: String -> IO ()
+printSuiteStart name =
+  putStrLn ("Suite " ++ name ++ ":")
+
+printSuiteEnd :: String -> TestReport -> IO ()
+printSuiteEnd name (TestReport success failures) =
+  putStrLn (linePrefix ++ sep ++ reportString)
+  where
+    linePrefix = "Suite " ++ name ++ ":"
+    sep = " "
+    reportString
+      | (failures == 0) = "All tests passed"
+      | otherwise = "TESTS FAILED: " ++ (show failures) ++ " (tests passed: " ++ (show success) ++ ")"
+
+printTreeRes :: Show t => Maybe t -> IO ()
+printTreeRes = maybe doNothing printTree
+  where doNothing = return ()
+        printTree tree = putStrLn ("> Generated tree: " ++ (show tree))
+
+-- Utilities to define tests
+
+basicTestLib :: (i -> TestResult) -> i -> String -> Test
+basicTestLib f input name = Test name (f input)
+
+basicAssertLib :: (Eq o, Show o) => (i -> o) -> o -> i -> String -> Test
+basicAssertLib f expectedOutput = basicTestLib assertResult
+  where assertResult input = detailedTestResult assertFailureText testPassed
+          where testOutput = f input
+                testPassed = (testOutput == expectedOutput)
+                assertFailureText = unlines [
+                  "> Expected: " ++ (show expectedOutput),
+                  "> Found:    " ++ (show testOutput)
+                  ]
+
+basicTestResult :: Bool -> TestResult
+basicTestResult True = Success
+basicTestResult False = Failure Nothing
+
+detailedTestResult :: String -> Bool -> TestResult
+detailedTestResult _ True = Success
+detailedTestResult explanation False = Failure (Just explanation)
 
 -- Test suites
-
-runAllTests :: IO ()
-runAllTests = runSuite parserSuite
 
 parserSuite = TestSuite "Parser" [
   valid "a",
@@ -72,31 +124,3 @@ validRegexTest expectedValidity inputString =
     wrapWithQuotes str = '"' : str ++ '"' : []
     isOrIsNot = bool "is" "is not"
     commonSuffix = "a valid regex"
-
--- Printers
-
-printTestResult :: Test -> IO ()
-printTestResult (Test name result) =
-  putStrLn ((testLinePrefix result) ++ sep ++ name)
-    where sep = " "
-          testLinePrefix True = "[OK]"
-          testLinePrefix False = "[XX]"
-
-printSuiteStart :: String -> IO ()
-printSuiteStart name =
-  putStrLn ("Suite " ++ name ++ ":")
-
-printSuiteEnd :: String -> TestReport -> IO ()
-printSuiteEnd name (TestReport success failures) =
-  putStrLn (linePrefix ++ sep ++ reportString)
-  where
-    linePrefix = "Suite " ++ name ++ ":"
-    sep = " "
-    reportString
-      | (failures == 0) = "All tests passed"
-      | otherwise = "TESTS FAILED: " ++ (show failures) ++ " (tests passed: " ++ (show success) ++ ")"
-
-printTreeRes :: Show t => Maybe t -> IO ()
-printTreeRes = maybe doNothing printTree
-  where doNothing = return ()
-        printTree tree = putStrLn ("> Generated tree: " ++ (show tree))
