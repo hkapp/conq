ea15152afe8e9193357e40f33682c934452a0ab5
Hugo Kapp <hugo.kapp@epfl.ch>
2020-04-08 21:07:44 +0200

    Major cleanup in the Parser basic operations

--- src/Parser.hs
+++ src/Parser.hs
@@ -5,6 +5,10 @@
 module Parser where
 
 import Data.Maybe
+import Data.Semigroup
+import Data.Foldable (find)
+import Control.Applicative
+import Data.Monoid hiding ((<>))
 
 -- Types
 
@@ -12,14 +16,70 @@ type Parser t = String -> ParseResult t
 
 data ParseResult t = Success t String | Failure
 
+-- ParseResult t is:
+-- a Monoid
+-- a SemiGroup
+-- a Foldable
+
+-- ParseResult t is not:
+-- a Monad
+-- a Functor
+-- a Traversable
+-- an Applicative
+-- an Alternative
+
+-- Functions we can implement:
+-- continueParsing :: ((a,String) -> ParseResult b) -> ParseResult a -> ParseResult b
+-- continueParsing :: (String -> a -> ParseResult b) -> ParseResult a -> ParseResult b
+-- ~~> Almost a Monad, but not quite
+-- getTreeWithDefault :: a -> ParseResult a -> a
+-- --> same as fromMaybe
+-- firstSuccess :: [ParseResult a] -> ParseResult a
+-- => Monoid.mconcat
+-- keepSuccess :: ParseResult a -> ParseResult a -> ParseResult a
+-- => MonadPlus.mplus, Alternative.(<|>)
+-- empty :: ParseResult a
+-- => Alternative.empty, Monoid.mempty
+-- return :: a -> ParseResult a
+-- => Applicative.pure, Monad.return, Maybe.Just
+-- parseAfter :: (a -> b -> b) -> Parser b -> ParseResult a -> ParseResult b
+-- parseAfter :: (a -> Parser b) -> ParseResult a -> ParseResult b
+
+-- ParseResult operations
+
+-- The ParseResult Semigroup returns the first success found
+instance Semigroup (ParseResult t) where
+  Failure <> b = b
+  a <> Failure = a
+  a@(Success _ _) <> b = a
+
+instance Monoid (ParseResult t) where
+  mempty = Failure
+  mappend = (<>)
+  mconcat = fromMaybe Failure . find isSuccess
+
+instance Functor ParseResult where
+  fmap f (Success a s) = Success (f a) s
+  fmap _ Failure = Failure
+
+extractFinalResult :: ParseResult t -> Maybe t
+extractFinalResult (Success finalTree []) = Just finalTree
+extractFinalResult _ = Nothing
+
+isSuccess :: ParseResult t -> Bool
+isSuccess (Success _ _) = True
+isSuccess _ = False
+
+continueParsing :: (a -> b -> c) -> ParseResult a -> Parser b -> ParseResult c
+continueParsing combineResults stem parseNext =
+  case stem of
+    Success ta remainingString -> fmap (combineResults ta) (parseNext remainingString)
+    Failure -> Failure
 
 -- Top-level parser operations
 
 parseString :: Parser t -> String -> Maybe t
-parseString parse input =
-  case parse input of
-    Success finalTree [] -> Just finalTree
-    _ -> Nothing
+parseString parse input = extractFinalResult (parse input)
 
 isValidString :: Parser t -> String -> Bool
 isValidString = isJust `appliedTo` parseString
@@ -28,26 +88,26 @@ isValidString = isJust `appliedTo` parseString
 
 -- Parser combinators
 
-parseAny :: [Parser t] -> Parser t
+parseAny :: (Foldable f) => f (Parser t) -> Parser t
+parseAny parsers input = foldMap ($ input) parsers
 
-parseAny (parseHead : remParsers) input = case parseHead input of
-  Success t s -> Success t s
-  Failure     -> parseAny remParsers input
+combineParsers :: (a -> b -> c) -> (Parser a -> Parser b -> Parser c)
+combineParsers combineResults parseFirst parseSecond input =
+  continueParsing combineResults (parseFirst input) parseSecond
 
-parseAny [] _ = Failure
-
+mapParser :: (a -> b) -> Parser a -> Parser b
+mapParser f p input = fmap f (p input)
 
 parseInSequence :: [Parser t] -> ([t] -> t) -> Parser t
-parseInSequence elems merge input = buildSeqRes $ foldl seqParse (Success [] input) elems
-  where
-    seqParse (Success prevTrees remainingInput) p = case p remainingInput of
-      Success t remainingAfterParse -> Success (t:prevTrees) remainingAfterParse
-      Failure -> Failure
-    seqParse Failure _ = Failure
-    buildSeqRes (Success treeStack remainingInput) = Success (merge (reverse treeStack)) remainingInput
-    buildSeqRes Failure = Failure
--- foldl = (a op b) op c
--- foldr = a op (b op c)
+parseInSequence parsers combineResults = mapParser combineResults (parseInSequence2 parsers)
+
+parseInSequence2 :: [Parser t] -> Parser [t]
+parseInSequence2 = foldr (combineParsers (:)) (\s -> Success [] s)
+
+parseInMonoidicStructure :: (Applicative f, Monoid (f t), Foldable f) => f (Parser t) -> Parser (f t)
+parseInMonoidicStructure = foldr (combineParsers combineResults) (Success mempty)
+  where combineResults val = mappend (pure val)
+
 
 parseRepetition :: Parser t -> ([t] -> t) -> Parser t
 parseRepetition parseRepExp combineRes input =
