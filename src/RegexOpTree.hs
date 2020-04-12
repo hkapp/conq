44d372d38fcc79b36ac469454c06da0f8d767d01
Hugo Kapp <hugo.kapp@epfl.ch>
2020-04-12 15:04:12 +0200

    Attempt a stack-based-fold approach for normalizing op trees

--- src/RegexOpTree.hs
+++ src/RegexOpTree.hs
@@ -2,7 +2,8 @@ module RegexOpTree where
 
 import RegexParser
 import Parser
-import Data.Set(Set, fromList)
+import Data.Set as Set (Set, fromList)
+import Data.Foldable (foldl')
 
 data RegexOpTree =
   RegexString String
@@ -14,17 +15,34 @@ data RegexOpTree =
 
 instance RegexTreeBuilder RegexOpTree where
   buildCharNode c = RegexString [c]
-  buildCharClassNode options = RegexCharClass (fromList (map extractChar options))
+  buildCharClassNode options = RegexCharClass $ Set.fromList (map extractChar options)
     where extractChar (RegexString (c:[])) = c
-  buildConcatNode opNodes = normalizedRegexSequence opNodes
+  buildConcatNode = normalizedRegexSequence
   buildAltNode = RegexAlternative
 
-buildRegexOpTree :: String -> Maybe RegexOpTree
-buildRegexOpTree = parseString regexParser
-
 normalizedRegexSequence :: [RegexOpTree] -> RegexOpTree
-normalizedRegexSequence = buildNormalized . foldr normalize []
+normalizedRegexSequence = buildNormalized . foldl' concatStringNodes []
+  where buildNormalized (node:[]) = node
+        buildNormalized nodeStack = RegexSequence (reverse nodeStack)
+        concatStringNodes ((RegexString prefix):moreNodes) (RegexString suffix) =
+          (RegexString (prefix ++ suffix)):moreNodes
+        concatStringNodes nodeStack newNode = newNode:nodeStack
+
+normalizedRegexSequence2 :: [RegexOpTree] -> RegexOpTree
+normalizedRegexSequence2 = buildNormalized . stackBasedFoldl stackAct id
   where buildNormalized (node:[]) = node
         buildNormalized nodes = RegexSequence nodes
-        normalize (RegexString prefix) ((RegexString suffix):moreNodes) = (RegexString (prefix ++ suffix)):moreNodes
-        normalize newNode someNodes = newNode:someNodes
+        stackAct (RegexString prefix) (RegexString suffix) = Replace $ RegexString (prefix ++ suffix)
+        stackAct _ newNode = Stack newNode
+
+data StackAction a = Ignore | Replace a | Stack a
+
+stackBasedFoldl :: (Foldable t) => (b -> a -> StackAction b) -> (a -> b) -> t a -> [b]
+stackBasedFoldl actionFor kickstart = reverse . foldl handleStack []
+  where
+    handleStack [] elem = [kickstart elem]
+    handleStack prevStack elem =
+      case actionFor (head prevStack) elem of
+        Ignore -> prevStack
+        Replace newHead -> newHead : (tail prevStack)
+        Stack newHead -> newHead : prevStack
